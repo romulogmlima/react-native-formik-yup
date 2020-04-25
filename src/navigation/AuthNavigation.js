2ecc63dcfc053bed20d3930f70b78b16fa51960e
romulogmlima <romulogadelhaml@gmail.com>
2020-04-24 22:45:34 -0300

    Initial commit

---
+++ src/navigation/AuthNavigation.js
@@ -0,0 +1,16 @@
+import {createStackNavigator} from 'react-navigation-stack';
+import Login from '../screens/Login';
+import Signup from '../screens/Signup';
+
+const AuthNavigation = createStackNavigator(
+  {
+    Login: {screen: Login},
+    Signup: {screen: Signup},
+  },
+  {
+    initialRouteName: 'Signup',
+    headerMode: 'none',
+  },
+);
+
+export default AuthNavigation;
